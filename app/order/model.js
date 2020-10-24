b197e4a31f80eff8bf76cfdc43714d3d544f52ac
sheptianbagja <sheptianbagjautama@gmail.com>
2020-10-25 02:40:08 +0700

    Fix api order subtotal undefined

--- app/order/model.js
+++ app/order/model.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const { model, Schema } = mongoose;
 const AutoIncrement = require('mongoose-sequence')(mongoose);
+
 const Invoice = require('../invoice/model');
 
 const orderSchema = Schema(
@@ -10,32 +11,32 @@ const orderSchema = Schema(
 			enum: [ 'waiting_payment', 'processing', 'in_delivery', 'delivered' ],
 			default: 'waiting_payment'
 		},
+
 		delivery_fee: {
 			type: Number,
 			default: 0
 		},
+
 		delivery_address: {
-			provinsi: { type: String, required: [ true, 'Provinsi harus diisi' ] },
-			kabupaten: { type: String, required: [ true, 'Kabupaten harus diisi' ] },
-			kecamatan: { type: String, required: [ true, 'kecamatan harus diisi' ] },
-			kelurahan: { type: String, required: [ true, 'kelurahan harus diisi' ] },
+			provinsi: { type: String, required: [ true, 'provinsi harus diisi.' ] },
+			kabupaten: { type: String, required: [ true, 'kabupaten harus diisi.' ] },
+			kecamatan: { type: String, required: [ true, 'kecamatan harus diisi.' ] },
+			kelurahan: { type: String, required: [ true, 'kelurahan harus diisi.' ] },
 			detail: { type: String }
 		},
+
 		user: {
 			type: Schema.Types.ObjectId,
-			ref: 'user'
+			ref: 'User'
 		},
+
 		order_items: [ { type: Schema.Types.ObjectId, ref: 'OrderItem' } ]
 	},
-	{
-		timestamps: true
-	}
+	{ timestamps: true }
 );
 
-// membuat field autoincrement
 orderSchema.plugin(AutoIncrement, { inc_field: 'order_number' });
 
-// menghitung total item dalam 1 data order
 orderSchema.virtual('items_count').get(function() {
 	return this.order_items.reduce((total, item) => {
 		return total + parseInt(item.qty);
@@ -43,9 +44,7 @@ orderSchema.virtual('items_count').get(function() {
 });
 
 orderSchema.post('save', async function() {
-	let sub_total = this.order_items.reduce((sum, item) => {
-		sum += item.price * item.qty;
-	}, 0);
+	let sub_total = this.order_items.reduce((sum, item) => (sum += item.price * item.qty), 0);
 
 	let invoice = new Invoice({
 		user: this.user,
